d33a5220f3f0525cbf765a9d2b77fb781214ffd2
Simon <simon.valat@sfr.fr>
2020-09-14 22:33:12 +0200

    Ajout du calendrier (v4.4.2), configuration en français

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  options : any;
+
+  ngOnInit(): void {
+        this.options = {
+            plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+            header: {
+                left: 'prev,next',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay',
+            },
+            buttonText: {
+              day:      'jour',
+              month:    'mois',
+              week:     'semaine'
+            },
+            locale: 'fr',
+            firstDay : 1
+        }
+  }
   title = 'MedPlanner';
 }
