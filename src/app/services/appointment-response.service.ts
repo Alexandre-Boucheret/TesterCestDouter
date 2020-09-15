0eaaba58e754c4d640365194aeae146e96300df1
NOGRETTE Florentin <florentin.nogrette@berger-levrault.com>
2020-09-15 09:53:25 +0200

    Post service AppointmentResponse + Environnement

--- src/app/services/appointment-response.service.ts
+++ src/app/services/appointment-response.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppointmentResponseService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createAppointmentResponse(appointmentResponse: JSON): Observable<JSON> {
+    return this.http.post<JSON>(environment.baseApiUrl + 'appointment-response', appointmentResponse);
+  }
+
 }
