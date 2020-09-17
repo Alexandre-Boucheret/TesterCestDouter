afaf4300ce68032f404428532a61c067b368bd57
NOGRETTE Florentin <florentin.nogrette@berger-levrault.com>
2020-09-17 08:25:18 +0200

    Style de l'application

--- src/app/components/fullcalendar/fullcalendar.component.ts
+++ src/app/components/fullcalendar/fullcalendar.component.ts
@@ -10,11 +10,11 @@ import { Appointment } from 'src/app/rest/restData';
   styleUrls: ['./fullcalendar.component.css']
 })
 export class FullcalendarComponent implements OnInit, OnChanges {
-  @Input() listAppointmentsPending : Array<Appointment>;
-  @Input() listAppointmentsBooked : Array<Appointment>;
+  @Input() listAppointmentsPending: Array<Appointment>;
+  @Input() listAppointmentsBooked: Array<Appointment>;
   events: any[];
-  options : any;
-  constructor() { 
+  options: any;
+  constructor() {
   }
 
   ngOnInit(): void {
@@ -31,10 +31,10 @@ export class FullcalendarComponent implements OnInit, OnChanges {
         month:    'mois',
         week:     'semaine'
       },
-      height: 650,
+      height: 600,
       locale: 'fr',
       firstDay : 1,
-      businessHours: {      
+      businessHours: {
         startTime : '7:30', // a start time (10am in this example)
         endTime : '19:00', // an end time (6pm in this example)
       },
@@ -45,25 +45,25 @@ export class FullcalendarComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.initEvents();    
-      
+    this.initEvents();
+
   }
 
-  initEvents(){
+  initEvents() {
     this.events = [];
     let color;
     this.listAppointmentsBooked.concat(this.listAppointmentsPending).forEach(a => {
-      if(a.status==='booked'){
-        color = "#333fff"
-      }else{
-        color="#a9a9b4"
+      if (a.status === 'booked') {
+        color = '#333fff';
+      } else {
+        color = '#a9a9b4';
       }
       this.events = [...this.events, {
-        "title": a.comment + " " +a.participant[0].actor.display,
-        "start": a.start,
-        "end": a.end,
-        "backgroundColor": color,
-        "borderColor" : color
+        title: a.participant[0].actor.display + ' ' + a.comment,
+        start: a.start,
+        end: a.end,
+        backgroundColor: color,
+        borderColor : color
       }];
     });
   }
